e828de2256844c6b2e2dffe2a0ac88ad14fa01ea
changhwa <changhwaoh.co@gmail.com>
2012-08-31 21:41:42 +0900

    bootstrap top부분

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,9 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+	res.render('index', { title: 'Express' });
 };
 
+exports.login = function(req,res){
+	alert('Login');
+}
