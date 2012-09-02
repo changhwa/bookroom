1b8da8ca372c5201c0a1844e084b08dd3c1e940b
changhwa <changhwaoh.co@gmail.com>
2012-09-03 07:44:33 +0900

    module test

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,15 @@
  * GET home page.
  */
 
+User = require('../lib/user').User;
+
 exports.index = function(req, res){
 	res.render('index', { title: 'Express' });
 };
 
 exports.login = function(req,res){
+	var aaa = User.userAdd();
+	alert(aaa);
 	res.writeHead(200, { 'Content-Type' : 'text/html'});
 	res.end('<h1> Test </h1>');
 }
