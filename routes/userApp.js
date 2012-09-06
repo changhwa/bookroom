be1485a01bcb263168f001e7e1506f4a80470242
changhwa <changhwaoh.co@gmail.com>
2012-09-06 21:55:17 +0900

    Login 부분 작업중

--- routes/userApp.js
+++ routes/userApp.js
@@ -3,12 +3,21 @@
 User = require('../lib/user').User;
 
 exports.login = function(req,res){
-	User.userAdd({
+	
+	return User.login({
 		id : req.body.id,
 		pw : req.body.pw
+	}, function (id, msg){
+		if(msg){
+			req.session.regenerate(function(err){
+				req.session.userId = id;  
+				console.log(req.session.userId);
+ 			});
+ 			res.render('index', {message: 'Login Success'});
+		} else{
+			res.render('index', {message: 'Login Fail'});
+		}
 	});
-	res.writeHead(200, { 'Content-Type' : 'text/html'});
-	res.end('<h1> Test </h1>');
 }
 
 
@@ -17,8 +26,8 @@ exports.register = function(req,res){
 	return User.userAdd({
 		id : req.body.id,
 		pw : req.body.pw
-	}, function(err){
-		if(err === 'Success'){
+	}, function(msg){
+		if(msg === 'Success'){
 			res.render('index' , { message: 'Register Success..' })
 		}else{
 			res.render('index' , { message: 'Register Fail..' })
